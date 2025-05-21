082877538b5ab5b2064087c9e61784caed0f51d4
rcalonso <r.alonsocortizo@gmail.com>
2025-05-21 13:19:32 +0200

    add asteroid

---
+++ asteroid.py
@@ -0,0 +1,37 @@
+import random
+
+import pygame
+
+from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
+
+class Asteroid(CircleShape):
+    containers = []
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self, screen):
+        pygame.draw.circle(
+            surface=screen, 
+            color=(255, 255, 255), 
+            center=self.position,
+            radius=self.radius,
+            width=2
+        )
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        angle = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        first_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        second_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        first_asteroid.velocity = self.velocity.rotate(angle) * 1.2
+        second_asteroid.velocity = self.velocity.rotate(-angle) * 1.2
+
+    def update(self, dt):
+        self.position += self.velocity * dt
